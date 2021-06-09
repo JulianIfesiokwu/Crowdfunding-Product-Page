d3e2d07f60b850c901c66fb37f2e5576c19bfa1c
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-06-09 14:44:36 +0100

    completed mobile view css, now working on adding dynamism using JavaScript

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// Variables 
+const hamburgerIcon = document.querySelector('.hamburger-menu');
+
+
+
+// Functions
+function showNavList() {
+    console.log(2)
+}
+
+
+
+
+// Event Listeners
+hamburgerIcon.addEventListener('click', showNavList);
