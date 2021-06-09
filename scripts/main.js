e6b6ae5e407d0d8f3c459077e9b96b959be2d626
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-06-09 21:11:06 +0100

    completely mobile functionality for hamburger dropdown list

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,32 @@
 // Variables 
-const hamburgerIcon = document.querySelector('.hamburger-menu');
+const hamburgerOpen = document.querySelector('.hamburger-open');
+const closeBtn = document.querySelector('.hamburger-close');
+const hamburgerLinks = document.querySelector('.hamburger-links');
 
 
 
 // Functions
 function showNavList() {
-    console.log(2)
+    //Display dropdown
+    hamburgerLinks.style.display = 'block';
+
+    //Display close btn and hide hamburger icon
+    hamburgerOpen.style.display = 'none';
+    closeBtn.style.display = 'block';
+}
+
+function hideNavList() {
+    // Hide closeBtn
+    closeBtn.style.display = 'none';
+
+    // Display Hamburger btn and hide list
+    hamburgerOpen.style.display = 'block';
+    hamburgerLinks.style.display = 'none';
 }
 
 
 
 
 // Event Listeners
-hamburgerIcon.addEventListener('click', showNavList);
+hamburgerOpen.addEventListener('click', showNavList);
+closeBtn.addEventListener('click', hideNavList)
