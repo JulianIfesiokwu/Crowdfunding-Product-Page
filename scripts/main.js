a1b4da093abcaa98d3bf2062c63af6cd5638a44b
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2021-06-14 12:03:09 +0100

    added contribution for pledges, reworked the structure of range bar

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,16 @@
 const hamburgerOpen = document.querySelector('.hamburger-open');
 const closeBtn = document.querySelector('.hamburger-close');
 const hamburgerLinks = document.querySelector('.hamburger-links');
+const bookmarkIcon = document.querySelector('svg');
+const ctaBtn = document.querySelector('.cta');
+const modalContainer = document.querySelector('#modal-container1');
+const closeModalBtn = document.querySelector('.close-modal');
+const bookmarkText = document.querySelector('.bookmark-text');
+const bookmarkBtn = document.querySelector('.icon-bookmark');
+
+// For slider
+const range = document.querySelector('.range');
+const track = document.querySelector('.track-inner');
 
 
 
@@ -24,9 +34,52 @@ function hideNavList() {
     hamburgerLinks.style.display = 'none';
 }
 
+function highlightBookmark() {
+    // edit bookmark color,background color, textContent and svg
+    bookmarkText.textContent = 'bookmarked';
+    bookmarkText.style.color = 'var(--dark-cyan)';
+    bookmarkBtn.style.backgroundColor = 'whitesmoke';
+    bookmarkIcon.style.filter = 'invert(37%) sepia(27%) saturate(1179%) hue-rotate(127deg) brightness(97%) contrast(90%)';
+}
 
+function displayPledgeOfferings() {
+    modalContainer.style.display = 'block';
+    // Stop body from scrolling
+    document.querySelector("body").style.overflow = 'hidden';
 
+}
+
+// Show pledge offerings
+function displayPledgeOfferings() {
+    modalContainer.style.display = 'block';
+}
+
+// Close Pledge Modal
+function closePledgeModal() {
+    modalContainer.style.display = 'none';
+    // document.querySelector("body").style.overflow = 'visible';
+}
+
+// update range slider with values
+const updateSlider = (value) => {
+    console.log(value)
+    track.style.left = `${value}%`
+    track.style.transform = `translate(-${value}%, -50%)`
+    track.style.width = `${value}%`
+}
+
+// range.oninput = (e) =>
+// console.log(e)
+//   updateSlider(e.target.value)
+
+updateSlider(89914) // Init value
+// console.log(range.value)
 
 // Event Listeners
 hamburgerOpen.addEventListener('click', showNavList);
-closeBtn.addEventListener('click', hideNavList)
+closeBtn.addEventListener('click', hideNavList);
+bookmarkBtn.addEventListener('click', highlightBookmark);
+ctaBtn.addEventListener('click', displayPledgeOfferings);
+if(closeModalBtn) {
+    closeModalBtn.addEventListener('click',closePledgeModal);
+}
